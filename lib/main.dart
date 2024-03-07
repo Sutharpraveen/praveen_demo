1f73977469e2c8484de90008f5dc1aca30b12100
akshaysoftssolution <akshaysoftssolution@outlook.com>
2024-03-08 00:30:59 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,13 @@ class MyApp extends StatelessWidget {
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
+        // try chsdsdfsdanging the seedColor in the colorScheme below to Colors.green
         // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // reload" bsdafsdfutton in a Flutter-supported IDE, or press "r" if you used
         // the command line to start the app).
         //
         // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
+        // state is notsadfsda lost during the reload. To reset the state, use hot
         // restart instead.
         //
         // This works for code too, not just values: Most code changes can be
